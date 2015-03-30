ba35c80482ceddc1bb1a66abac5c531b2592572f
Joshua Lee <10leej@gmail.com>
2015-03-30 03:56:25 -0400

    remove stuff
    removed unnecessary code

--- core.lua
+++ core.lua
@@ -1,22 +1,9 @@
 local _, cfg = ... --import config
-if not CompactRaidFrameManager then LoadAddOn("Blizzard_CompactRaidFrames") end --hacky method because break things
 
 --[[
 Cause I wanted to do it!
  --10leej
 ]]
-
-local function CreateBackdrop(frame)
-	frame:SetBackdrop({bgFile = cfg.backdrop, edgeFile = "Interface\\Buttons\\WHITE8x8", edgeSize = cfg.pixelbordersize, 
-		insets = {top = 2, left = 2, bottom = 2, right = 2}})
-	frame:SetBackdropColor(unpack(cfg.bColor))
-	frame:SetBackdropBorderColor(unpack(cfg.bColor))
-	if isBeautiful then
-		frame:CreateBeautyBorder(cfg.border.size.large)
-		frame:SetBeautyBorderTexture(cfg.border.texture)
-		frame:SetBeautyBorderColor(unpack(cfg.border.color))
-	end
-end
 
 local function Borderize(frame) -- style our frame
     if frame then
@@ -30,67 +17,6 @@ _G[n.."HeightSlider"],
 _G[n.."WidthSlider"] 
 h:SetMinMaxValues(1,150) --height
 w:SetMinMaxValues(1,150) --width
-
---Skin the RaidManager
-
---hide stuff
-local buttons = {
-	CompactRaidFrameManagerDisplayFrameFilterOptionsFilterRoleTank,
-	CompactRaidFrameManagerDisplayFrameFilterOptionsFilterRoleHealer,
-	CompactRaidFrameManagerDisplayFrameFilterOptionsFilterRoleDamager,
-	CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup1,
-	CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup2,
-	CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup3,
-	CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup4,
-	CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup5,
-	CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup6,
-	CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup7,
-	CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup8,
-	CompactRaidFrameManagerDisplayFrameLeaderOptionsInitiateRolePoll,
-	CompactRaidFrameManagerDisplayFrameLeaderOptionsInitiateReadyCheck,
-	CompactRaidFrameManagerDisplayFrameLeaderOptionsRaidWorldMarkerButton,
-	CompactRaidFrameManagerDisplayFrameLockedModeToggle,
-	CompactRaidFrameManagerDisplayFrameHiddenModeToggle,
-	CompactRaidFrameManagerDisplayFrameConvertToRaid
-}
-CompactRaidFrameManagerDisplayFrameLeaderOptionsRaidWorldMarkerButton.SetNormalTexture = function() end
-for _, button in pairs(buttons) do
-	for i = 1, 3 do
-		select(i, button:GetRegions()):SetAlpha(0)
-	end
-end
-
-for i = 1, 8 do
-	select(i, CompactRaidFrameManager:GetRegions()):Hide()
-end
-select(1, CompactRaidFrameManagerDisplayFrameFilterOptions:GetRegions()):Hide()
-select(1, CompactRaidFrameManagerDisplayFrame:GetRegions()):Hide()
-select(4, CompactRaidFrameManagerDisplayFrame:GetRegions()):Hide()
-
-local bd = CreateFrame("Frame", nil, CompactRaidFrameManager)
-bd:SetPoint("TOPLEFT", CompactRaidFrameManager, "TOPLEFT")
-bd:SetPoint("BOTTOMRIGHT", CompactRaidFrameManager, "BOTTOMRIGHT", -9, 9)
-
---now we skin it and yes it's really this simple :)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameFilterOptionsFilterRoleTank)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameFilterOptionsFilterRoleHealer)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameFilterOptionsFilterRoleDamager)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup1)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup2)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup3)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup4)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup5)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup6)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup7)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameFilterOptionsFilterGroup8)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameLeaderOptionsInitiateRolePoll)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameLeaderOptionsInitiateReadyCheck)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameLeaderOptionsRaidWorldMarkerButton)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameLockedModeToggle)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameHiddenModeToggle)
-CreateBackdrop(CompactRaidFrameManagerDisplayFrameConvertToRaid)
-CreateBackdrop(CompactRaidFrameManager)
-CompactRaidFrameManagerToggleButton:SetNormalTexture("Interface\\AddOns\\BobUI\\media\\buttons\\RaidPanel-Toggle")
 
 --This is how you skin the default raid frames
 f = CreateFrame("Frame")
